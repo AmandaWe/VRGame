5c38ceeb0867c8d34864125be29c3ad8d893b188
VCT <VCT136@Gmail.com>
2020-11-28 21:48:10 +0100

    some progress on the ar thing

---
+++ ARgoraphobia/Assets/Scripts/PlacementIndicatorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicatorController : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<ARRaycastHit> hitsList = new List<ARRaycastHit>();
+        raycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hitsList, TrackableType.Planes);
+        
+        if (hitsList.Count > 0) {
+            transform.position = hitsList[0].pose.position;
+            transform.rotation = hitsList[0].pose.rotation;
+
+            if (!gameObject.activeInHierarchy) {
+                gameObject.SetActive(true);
+            }
+        }
+    }
+}
